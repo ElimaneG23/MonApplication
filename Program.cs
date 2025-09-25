146fe6f171faf727c9d4ae779bacec20c4c190fc
ElimaneG23 <gelimane23@gmail.com>
2025-09-25 14:44:32 +0000

    voici l'exercice 5

--- Program.cs
+++ Program.cs
@@ -1,18 +1,22 @@
 ﻿using System;
-namespace ArayDemo
+namespace ExoCsharp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int n = 7;
+            
 
-            int f = 1;
-            for (int i = 1; i <= n; i++)
-            {
-                f *= i;
-            }
-            Console.WriteLine("La Factorielle de "+n+" est :" + f);
+        }
+    }
+    class Student
+    {
+        string Nom = "Elimane";
+        int Age = 35;
+
+        void AfficherInfos()
+        {
+            Console.WriteLine("Nom: " + Nom + " est age de" + Age + " ans");
         }
     }
 }
